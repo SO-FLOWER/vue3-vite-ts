f13d92671f48354e0df71c3235352250c6e61fd7
Jsons <1006705381@qq.com>
2023-10-14 20:58:36 +0800

    feat: 配置husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,33 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
+  env: {
+    browser: true,
+    es2021: true
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:vue/vue3-essential",
-    "plugin:prettier/recommended",
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended'
   ],
-  "overrides": [
+  overrides: [
     {
-      "env": {
-        "node": true
+      env: {
+        node: true
       },
-      "files": [
-        ".eslintrc.{js,cjs}"
-      ],
-      "parserOptions": {
-        "sourceType": "script"
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
       }
     }
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "parser": "@typescript-eslint/parser",
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   },
-  "plugins": [
-    "@typescript-eslint",
-    "vue"
-  ],
-  "rules": {
+  plugins: ['@typescript-eslint', 'vue'],
+  rules: {
+    'no-undef': 0,
     'no-var': 'error', // 要求使用 let 或 const 而不是 var
     'no-multiple-empty-lines': ['warn', { max: 1 }], // 不允许多个空行
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
